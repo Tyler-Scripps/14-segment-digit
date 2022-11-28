9ab0909068881a3fc8bb5e431cf03992dd15ff0b
Tyler-Scripps <tylerscripps@gmail.com>
2022-11-27 21:10:44 -0700

    started adding segment control

--- fourteen_segment_digit.cpp
+++ fourteen_segment_digit.cpp
@@ -1,5 +1,33 @@
 #include "fourteen_segment_digit.h"
 
+// the leds are wired up as so
+//
+//      9
+//  1_______2
+//  |\  |10/|
+// 8| \ | / |3
+//  |  \|/  |
+// 14——— ———13
+//  |  /|\  |
+// 7| / | \ |4
+//  |/  |11\| 
+//  6———————5
+//      12
+
+// the alphanumeric_segs array is using a clockwise labeling scheme
+
+//      a
+//  i_______k
+//  |\  |j /|
+// f| \ | / |b
+//  |  \|/  |
+//  g———————h
+//  |  /|\  |
+// e| / | \ |c
+//  |/  |m \| 
+//  n———————l
+//      d
+
 fourteen_segment_digit::fourteen_segment_digit() {
 
 }
@@ -10,6 +38,35 @@ int fourteen_segment_digit::begin(CRGB newPtr[], uint8_t longCount, uint8_t shor
     this->numShortSegment = shortCount;
     this->numMiddleGap = newGap;
     this->totalLEDs = 4*longCount + 9*shortCount;
+
+    this->segments[0][0] = ; //a (9)
+    this->segments[0][1] = ; //a (9)
+    this->segments[1][0] = 2*longCount + 0*shortCount; //b (3)
+    this->segments[1][1] = 2*longCount + 0*shortCount + shortCount - 1; //b (3)
+    this->segments[2][0] = 2*longCount + 1*shortCount; //c (4)
+    this->segments[2][1] = 2*longCount + 1*shortCount + shortCount - 1; //c (4)
+    this->segments[3][0] = ; //d (12)
+    this->segments[3][1] = ; //d (12)
+    this->segments[4][0] = ; //e (7)
+    this->segments[4][1] = ; //e (7)
+    this->segments[5][0] = ; //f (8)
+    this->segments[5][1] = ; //f (8)
+    this->segments[6][0] = ; //g (14)
+    this->segments[6][1] = ; //g (14)
+    this->segments[7][0] = ; //h (13)
+    this->segments[7][1] = ; //h (13)
+    this->segments[8][0] = 0; //i (1)
+    this->segments[8][1] = 0*longCount + 0*shortCoutnt + longCount - 1; //i (1)
+    this->segments[9][0] = ; //j (10)
+    this->segments[9][1] = ; //j (10)
+    this->segments[10][0] = 1*longCount + 0*shortCount; //k (2)
+    this->segments[10][1] = 1*longCount + 0*shortCount + longCount - 1; //k (2)
+    this->segments[11][0] = 2*longCount + 2*shortCount; //l (5)
+    this->segments[11][1] = 2*longCount + 2*shortCount + longCount - 1; //l (5)
+    this->segments[12][0] = ; //m (11)
+    this->segments[12][1] = ; //m (11)
+    this->segments[13][0] = 3*longCount + 2*shortCount; //n (6)
+    this->segments[13][1] = ; //n (6)
 
     return this->totalLEDs;
 }
--- fourteen_segment_digit.h
+++ fourteen_segment_digit.h
@@ -9,10 +9,13 @@ private:
     uint8_t numShortSegment = 0;    //how many leds are in a short (horizontal/vertical) segment
     uint8_t numMiddleGap = 0;   //number of leds between the two half length segments in the middle
     uint8_t totalLEDs = 0;  //total leds in strip
+    uint8_t segments[14][2];
     CRGB* stripPtr;
 public:
     fourteen_segment_digit();
     int begin(CRGB newPtr[], uint8_t longCount, uint8_t shortCount, uint8_t newGap);
+    void setSegment(uint8_t segmentNum, uint8_t r,, uint8_t g, uint8_t b);
+    void setSegments(uint16_t segmmentBits, uint8_t r,, uint8_t g, uint8_t b);
     void setChar(char newChar, uint8_t r, uint8_t g, uint8_t b);
     void erase();
     void fill(uint8_t r, uint8_t g, uint8_t b);
