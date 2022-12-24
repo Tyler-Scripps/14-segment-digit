575b34675493aba4df9dbb01be83d167c2bbff31
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-12-23 19:01:24 -0700

    added leftright and datatop to begin

--- fourteen_segment_digit.h
+++ fourteen_segment_digit.h
@@ -13,7 +13,7 @@ private:
     CRGB* stripPtr;
 public:
     fourteen_segment_digit();
-    int begin(CRGB newPtr[], uint8_t longCount, uint8_t shortCount, uint8_t newGap);
+    int begin(CRGB newPtr[], uint8_t longCount, uint8_t shortCount, uint8_t newGap, bool leftRight, bool dataTop);
     void setSegment(uint8_t segmentNum, uint8_t r, uint8_t g, uint8_t b);
     void setSegments(uint16_t segmentBits, uint8_t r, uint8_t g, uint8_t b);
     void setChar(char newChar, uint8_t r, uint8_t g, uint8_t b);
